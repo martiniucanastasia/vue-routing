d237fd0eed57adf6a06e96a8df82e108432acdd8
amartiniuc <amartiniuc@griddynamics.com>
2023-04-04 10:03:53 +0300

    Add nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import CarView from "../views/CarView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
+import DealerView from "../views/DealerView.vue";
+import ManufacturerView from "../views/ManufacturerView.vue";
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +23,16 @@ export const router = createRouter({
       path: "/car/:id",
       name: "car",
       component: CarView,
+      children: [
+        {
+          path: "dealer",
+          component: DealerView,
+        },
+        {
+          path: "manufacturer",
+          component: ManufacturerView,
+        },
+      ],
     },
     {
       path: "/:pathMatch(.*)*",
